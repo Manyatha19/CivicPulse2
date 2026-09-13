824cd0575deb12af9acf75e9504509c961be7e77
Manyatha19 <jeevanerughadindla@gmail.com>
2026-09-13 20:14:20 +0530

    Add Navbar component with navigation links

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,1 +1,18 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <span className="logo-icon">C</span>
+        <span>CivicPulse</span>
+      </div>
 
+      <div className="nav-links">
+        <a href="#about">About</a>
+        <a href="#help">How it works</a>
+        <button className="login-btn">Log in</button>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
